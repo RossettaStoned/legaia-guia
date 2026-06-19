f323cf755d40d85349734aded414b2ea79e10f72
Nico Vargas <124620757+RossettaStoned@users.noreply.github.com>
2026-06-19 13:22:08 -0400

    Add files via upload
    Corrección para el home

--- sw.js
+++ sw.js
@@ -1,23 +1,30 @@
-const CACHE = 'legaia-v2';
-const FILES = [
-  '/legaia-guia/',
-  '/legaia-guia/index.html',
-  '/legaia-guia/manifest.json',
-  '/legaia-guia/icon.svg'
-];
+const CACHE = 'legaia-v4';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon.svg'
+    ])).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-    const clone = res.clone();
-    caches.open(CACHE).then(c => c.put(e.request, clone));
-    return res;
-  })));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
 });
